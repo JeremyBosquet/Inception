ffe22fceaaba42705d69cc92eec70637c562c355
Jeremy Bosquet - VPS <jbosquet@student.42lyon.fr>
2022-07-20 10:56:58 +0200

    update wordpress and use .env for wordpress database

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -4,12 +4,15 @@
 sed -i 's=/run/php/php7.3-fpm.sock=9000=' /etc/php/7.3/fpm/pool.d/www.conf
 
 #Replace mysql database credentials with value from .env
-sed -i 's/database_name_here/$MYSQL_DB_NAME/g' /var/www/html/wp-config-sample.php
-sed -i 's/username_here/$MYSQL_DB_USER/g' /var/www/html/wp-config-sample.php
-sed -i 's/password_here/$MYSQL_PASSWORD/g' /var/www/html/wp-config-sample.php
+sed -i "s/database_name_here/$MYSQL_DB_NAME/g" /var/www/html/wp-config-sample.php
+sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/wp-config-sample.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/html/wp-config-sample.php
+sed -i "s/localhost/mariadb/g" /var/www/html/wp-config-sample.php
 
 #Change file name to wp-config.php
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 #Change permissions files
 chown -Rf www-data:www-data /var/www/html
+
+exec "$@"
